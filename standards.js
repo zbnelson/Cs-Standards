e784bf2443c2168a2f656c84055ac16e7943376d
Zachary Nelson <zbnelson@ksu.edu>
2020-06-18 22:26:38 +0000

    initial commit

---
+++ standards.js
@@ -0,0 +1,57 @@
+/** standards.js
+ * @author Nathan H. Bean
+ * This file contains the standards you will be rendering on your HTML page.  
+ * You should not make any changes to this file.
+ */
+
+var standards = [
+    {
+        identifier: "PK.CS.D.01",
+        statement: "With guidance, demonstrate how to operate a computing device.",
+        description: "People use computing devices to perform a variety of tasks accurately and quickly. With guidance, students should be able to complete this task",
+        subconcept: "Devices",
+        practices: ["7. Communicating about computing"]
+    },
+    {
+        identifier: "MG.IC.C.01",
+        statement: "Describe the trade-offs associated with computing technologies (e.g. automation), explaining their effects on economies and global societies, and explore careers related to the field of computer science.",
+        description: "Advancements in computer technology are neither wholly positive nor negative. However, the ways that people use computing technologies have tradeoffs. Students should consider current events related to broad ideas, including privacy, communication, and automation. For example, driverless cars can increase convenience and reduce accidents, but they are also susceptible to hacking. The emerging industry will reduce the number of taxi and shared-ride drivers, but will create more software engineering and cybersecurity jobs",
+        subconcept: "Culture",
+        practices: ["7. Communicating about computing"]
+    },
+    {
+        identifier: "4.DA.S.01",
+        statement: "Classify different storage locations (physical, shared, or cloud) based on the type of file, storage requirements, and sharing requirements.",
+        description: "Different Software tools used to access data may store the data differently. The type of data being stored and the level of detail represented by that data affect the storage requirements (file size, availability, and available memory). Music, images, video, and text require different amounts of storage. Video will often require more storage than music or images alone because video combines both.",
+        subconcept: "Storage",
+        practices: ["7. Communicating about computing"]
+    },
+    {
+        identifier: "4.AP.A.01",
+        statement: "Analyze and refine multiple algorithms for the same task.",
+        description: "Different algorithms can achieve the same result; however, sometimes one algorithm might be more suited for a particular situation. Students should be able to look at different ways to solve a problem or complete a task and decide which would be the best solution. For example, students could write different algorithms to draw a regular polygon and determine which algorithm would be the easiest to modify or repurpose to draw a different polygon.",
+        subconcept: "Algorithms",
+        practices: ["3. Recognizing and Defining Computational Problems", "6. Testing"]
+    },
+    {
+        identifier: "4.AP.PD.02",
+        statement: "Use proper citations and document when ideas are borrowed and changed for their own use (e.g., using pictures created by others, using music created by others, remixing programming projects).",
+        description: "Students should identify instances of remixing, when ideas are borrowed and iterated upon, and credit the original creator. Students should also consider common licenses that place limitations or restrictions on the use of computational artifacts, such as images and music downloaded from the Internet. At this stage, attribution should be written in the format required by the teacher and should always be included on any programs shared online. Students work through how to create citations for various borrowed resources. both writing citations manually then introducing websites that assist in citation creation",
+        subconcept: "Program Development",
+        practices: ["5. Creating computational artifacts", "7. Communicating about computing"]
+    },
+    {
+        identifier: "5.AP.PD.03",
+        statement: "Analyze, debug (identify/fix errors), and create a program that includes sequencing, repetition and variables in a programming language.",
+        description: "As students develop programs they should continuously test those programs to see that they do what was expected and fix (debug), any errors. Students should also be able to successfully debug simple errors in programs created by others.",
+        subconcept: "Program Development",
+        practices: ["1. Fostering an Inclusive Computing Culture", "2. Collaborating Around Computing", "6. Testing and Refining Computational Artifacts"]
+    },
+    {
+        identifier: "MG.IC.SLE.01",
+        statement: "Discuss the social impacts and ethical considerations associated with cybersecurity, including the positive and malicious purposes of hacking.",
+        description: "Sharing information online can help establish, maintain, and strengthen connections between people. For example, it allows artists and designers to display their talents and reach a broad audience. However, security attacks often start with personal information that is publicly available online. Social engineering is based on tricking people into revealing sensitive information and can be thwarted by being wary of attacks, such as phishing and spoofing.",
+        subconcept: "Safety, Law, & Ethics",
+        practices: ["7. Communicating about computing"]
+    }
+]
